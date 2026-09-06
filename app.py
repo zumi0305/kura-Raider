4ed3d7209151e16b16ec9ccd007aaac2706631a6
zumi0305 <ryuki.24.03.05@gmail.com>
2026-09-07 07:12:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -645,4 +645,5 @@ def export_tokens():
     return jsonify({'tokens': state.token_info})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
